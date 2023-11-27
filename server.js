3c33de1a1aeeae672b4459855f8226841b1f5e33
umerkhayyam91 <umerkhayyam91@gmail.com>
2023-11-27 17:50:13 +0500

    cors

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
 app.use(cookieParser());
 const dbConnect = require("./database/index");
-const ErrorHandler = require('./middlewares/errorHandler');
-const {PORT} = require("./config/index")
+const ErrorHandler = require("./middlewares/errorHandler");
+const { PORT } = require("./config/index");
 app.use(express.json({ limit: "50mb" }));
 
-app.use(cors());
 const labRouter = require("./routes/laboratory");
 
 app.use(labRouter);
 
- dbConnect();
-app.use(ErrorHandler)
-app.listen(PORT , ()=>{
-    console.log('server running')
-})
+dbConnect();
+app.use(ErrorHandler);
+app.listen(PORT, () => {
+  console.log("server running");
+});
