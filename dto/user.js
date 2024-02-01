1d10bf98dc96c08a0acbb75ae3b7f9810cf49401
umerkhayyam91 <umerkhayyam91@gmail.com>
2024-02-01 17:57:13 +0500

    gender added to user schema

--- dto/user.js
+++ dto/user.js
@@ -2,6 +2,7 @@ class UserDTO{
     constructor(user){
         this._id = user._id;
         this.name = user.name;
+        this.gender = user.gender;
         this.email = user.email;
         this.phone = user.phone;
         this.password = user.password;
