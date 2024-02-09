a06335f52dcc41e00ec64de3b5de5531a3bf15d8
umerkhayyam91 <umerkhayyam91@gmail.com>
2024-02-09 16:03:25 +0500

    minor change

--- controller/Rent A Car/vehicleRequestController.js
+++ controller/Rent A Car/vehicleRequestController.js
@@ -2,68 +2,68 @@ const acceptedRequests = require("../../models/Rent A Car/acceptedRequests");
 const vehicleRequest = require("../../models/Rent A Car/vehicleRequest");
 
 const vehicleRequestController = {
-    async getAllRequests(req, res, next) {
-        try {
-            const page = parseInt(req.query.page) || 1; // Get the page number from the query parameter
-            const requestsPerPage = 10;
-            const rentACarId= req.user._id;
-            const totalVehicle = await vehicleRequest.countDocuments({
-              rentACarId,
-            }); // Get the total number of posts for the user
-            const totalPages = Math.ceil(totalVehicle / requestsPerPage); // Calculate the total number of pages
-      
-            const skip = (page - 1) * requestsPerPage; // Calculate the number of posts to skip based on the current page
-      
-            const requests = await vehicleRequest.find({ rentACarId })
-              .skip(skip)
-              .limit(requestsPerPage);
-            let previousPage = page > 1 ? page - 1 : null;
-            let nextPage = page < totalPages ? page + 1 : null;
-            return res.status(200).json({
-              requests: requests,
-              auth: true,
-              previousPage: previousPage,
-              nextPage: nextPage,
-            });
-          } catch (error) {
-            return next(error);
-          }
-      },
-      async addRequests(req,res,next){
-        const { userId, userName, vehicleModel} = req.body;
-        rentACarId = req.user._id;
-        const request = new vehicleRequest({
-          vehicleId,
-          rentACarId,
-          userId,
-          userName,
-          vehicleModel,
-        })
-        await request.save();
-         res.status(200).json({
-          request: request,
-          auth: true,
-        });
-      },
-async getRequest(req,res,next){
+  async getAllRequests(req, res, next) {
+    try {
+      const page = parseInt(req.query.page) || 1; // Get the page number from the query parameter
+      const requestsPerPage = 10;
+      const rentACarId = req.user._id;
+      const totalVehicle = await vehicleRequest.countDocuments({
+        rentACarId,
+      }); // Get the total number of posts for the user
+      const totalPages = Math.ceil(totalVehicle / requestsPerPage); // Calculate the total number of pages
+
+      const skip = (page - 1) * requestsPerPage; // Calculate the number of posts to skip based on the current page
+
+      const requests = await vehicleRequest.find({ rentACarId })
+        .skip(skip)
+        .limit(requestsPerPage);
+      let previousPage = page > 1 ? page - 1 : null;
+      let nextPage = page < totalPages ? page + 1 : null;
+      return res.status(200).json({
+        requests: requests,
+        auth: true,
+        previousPage: previousPage,
+        nextPage: nextPage,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  async addRequests(req, res, next) {
+    const { userId, userName, vehicleModel } = req.body;
+    rentACarId = req.user._id;
+    const request = new vehicleRequest({
+      vehicleId,
+      rentACarId,
+      userId,
+      userName,
+      vehicleModel,
+    })
+    await request.save();
+    res.status(200).json({
+      request: request,
+      auth: true,
+    });
+  },
+  async getRequest(req, res, next) {
 
     try {
-        const requestId = req.user._id;
-        const requests = await vehicleRequest.find({ requestId , status: "pending"});
-        if (requests.length == 0) {
-          const error = new Error(" No Request for Vehicle found!");
-          error.status = 404;
-          return next(error);
-        }
-        return res.status(200).json({ requests });
-      } catch (error) {
+      const requestId = req.user._id;
+      const requests = await vehicleRequest.find({ requestId, status: "pending" });
+      if (requests.length == 0) {
+        const error = new Error(" No Request for Vehicle found!");
+        error.status = 404;
         return next(error);
       }
+      return res.status(200).json({ requests });
+    } catch (error) {
+      return next(error);
+    }
 
 
-},
+  },
 
-async acceptRequest(req, res, next) {
+  async acceptRequest(req, res, next) {
     try {
       const requestId = req.query.requestId;
       const requests = await vehicleRequest.findById(requestId);
@@ -72,7 +72,7 @@ async acceptRequest(req, res, next) {
         error.status = 404;
         return next(error);
       }
-      
+
       requests.status = "accept";
       await requests.save();
 
@@ -82,7 +82,7 @@ async acceptRequest(req, res, next) {
         rentACarId: requests.rentACarId,
         userId: requests.userId,
         userName: requests.userName,
-        vehicleModel:requests.vehicleModel,
+        vehicleModel: requests.vehicleModel,
       });
       await onRequest.save();
       return res.status(200).json({
@@ -95,9 +95,9 @@ async acceptRequest(req, res, next) {
   },
   async rejectRequest(req, res, next) {
     try {
-        const requestId = req.query.requestId;
-        const requests = await vehicleRequest.findById(requestId);
-        if (!requests) {
+      const requestId = req.query.requestId;
+      const requests = await vehicleRequest.findById(requestId);
+      if (!requests) {
         const error = new Error(" Request for Vehicle not found!");
         error.status = 404;
         return next(error);
@@ -112,5 +112,5 @@ async acceptRequest(req, res, next) {
     }
   },
 }
-  
-  module.exports = vehicleRequestController;
+
+module.exports = vehicleRequestController;
