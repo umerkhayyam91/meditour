3082f35e200dfa9d3023203ea667e7be17c50555
umerkhayyam91 <umerkhayyam91@gmail.com>
2024-02-14 21:05:39 +0500

    addOrder route added to users  lab

--- models/Laboratory/labOrder.js
+++ models/Laboratory/labOrder.js
@@ -1,45 +1,55 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  orderId: {
-    type: String,
-    required: true,
+const orderSchema = new mongoose.Schema(
+  {
+    labId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Laboratory",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+    },
+    testIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+    orderId: {
+      type: String,
+      required: true,
+    },
+    preference: {
+      type: String,
+      enum: ["labVisit", "homeSample"],
+    },
+    currentLocation: {
+      type: String,
+    },
+    prescription: {
+      type: String,
+    },
+    patientName: {
+      type: String,
+      required: true,
+    },
+    MR_NO: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "inProcess", "completed"],
+      default: "pending",
+    },
+    results: {
+      type: String,
+    },
   },
-  testCode: {
-    type: String,
-    required: true,
-  },
-  labId: {
-    type: mongoose.Schema.Types.ObjectId,
-        ref: "Laboratory",
-  },
-  testName: {
-    type: String,
-    required: true,
-  },
-  patientName: {
-    type: String,
-    required: true,
-  },
-  MR_NO: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "inProcess", "completed"],
-  },
-  results: {
-    type: String,
-    required: true,
-  },
-},
-{
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("LabOrder", orderSchema, "lab orders");
