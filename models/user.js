e7886afaf2c031c0d8a452355e120460c352ab8d
umerkhayyam91 <umerkhayyam91@gmail.com>
2024-02-14 14:00:52 +0500

    multiple changes

--- models/user.js
+++ models/user.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
-    userName: { type: String },
-    email: { type: String, required: true },
-    loc: {
-      type: [Number], // Array of two numbers: [longitude, latitude]
-      index: { type: '2dsphere', sparse: true }, // Use an object for index definition
-      required: true,
-  }
+const userSchema = new Schema({
+  userName: { type: String },
+  email: { type: String, required: true },
+  loc: {
+    type: [Number], // Array of two numbers: [longitude, latitude]
+    index: { type: "2dsphere", sparse: true }, // Use an object for index definition
+    required: true,
   },
-);
-userSchema.index({ loc: '2dsphere' });
+});
+userSchema.index({ loc: "2dsphere" });
 
 module.exports = mongoose.model("User", userSchema, "users");
